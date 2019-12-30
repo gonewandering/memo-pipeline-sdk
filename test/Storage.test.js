930f7d3bda581f74c54ab906197f9598c478b2cd
Lindsay Silver <lgsilver@gmail.com>
2019-12-29 22:12:57 -0800

    Added state class

--- test/Storage.test.js
+++ test/Storage.test.js
@@ -7,7 +7,7 @@ describe('Storage', () => {
       let test, storage
 
       try {
-        let storage = new Storage('123', '345')
+        let storage = new Storage('z123', 'z345')
         test = true
       } catch(e) {
         test = false
@@ -21,19 +21,18 @@ describe('Storage', () => {
   describe('Set()', () => {
     it('Should save a file successfully', async () => {
       let test
-      let storage = new Storage('123', '345')
+      let storage = new Storage('z123', 'z345')
       let buff = 'This is a zap'
 
       try {
-        await storage.set('test', buff)
+        await storage.set('test2', buff)
         test = true
       } catch(e) {
         test = false
-        console.log(e)
       }
 
       assert(test)
-    })
+    }).timeout(5000)
   })
 
   describe('Get()', () => {
